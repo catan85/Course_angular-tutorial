60876e7ef968973ccb1dad741406fc726450cae6
catan85 <catan85@gmail.com>
2017-05-09 18:04:55 +0200

    caricamento di un component dinamico specificando un parametro nell'indirizzo di route

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {Routes, RouterModule} from '@angular/router'
+import { Routes, RouterModule} from '@angular/router'
 
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
+import { ServersComponent } from './servers/servers.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
@@ -18,6 +18,7 @@ import { ServersService } from './servers/servers.service';
 const appRoutes: Routes = [
 { path: '', component: HomeComponent },
 { path: 'users', component: UsersComponent },
+{ path: 'users/:id', component: UserComponent },
 { path: 'servers', component: ServersComponent }
 ];
 
