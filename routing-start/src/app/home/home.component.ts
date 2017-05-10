d85e1b66c326da222b4b76083da678c764428f4d
catan85 <catan85@gmail.com>
2017-05-10 22:39:37 +0200

    proprietà CanActivate del routing, serve a dare l'abilitazione ad una route e relativo componente. Poco chiara la gestione asincrona della cosa tramite i Promise. Sembra una specie di callback (che nell'esempio è attivato dopo un ritardo per simulare un login su di un server)

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router} from '@angular/router';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +8,9 @@ import {Router} from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router
+    private authService : AuthService) { }
 
   ngOnInit() {
   }
@@ -15,6 +18,14 @@ export class HomeComponent implements OnInit {
   onLoadServer(id:number){
   	// complex calculations
   	this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
+
+  onLogin(){
+    this.authService.login();
+  }
+
+  onLogout(){
+    this.authService.logout();
   }
 
 }
