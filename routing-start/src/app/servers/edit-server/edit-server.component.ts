aa4af1a87b0d85a83a341cc0d5d22cb6b4b00017
ancattaneo <ancattaneo@marzoli.it>
2017-05-11 13:20:42 +0200

    continuano le modifiche per applicare can-deactivate, in teoria avrei finito ma non funziona

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute , Params, Router} from '@angular/router';
 import { ServersService } from '../servers.service';
-
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
@@ -44,4 +45,17 @@ export class EditServerComponent implements OnInit {
     this.router.navigate(['../'],{relativeTo: this.route})
   }
 
+  canDeactivate() : Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit){
+      return true;
+    }
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status)
+      && !this.changesSaved)
+    {
+      return confirm('Do you want to discard changes?');
+    }else
+    {
+      return true;
+    }
+  }
 }
