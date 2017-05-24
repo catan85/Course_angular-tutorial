5c0e49b11f7d9c7dbba5101acf0b0908e3a6e15b
catan85 <catan85@gmail.com>
2017-05-24 19:25:55 +0200

    compito form completo

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') myForm : NgForm
 
+  defaultLevel = 'advanced';
+
+  submitForm()
+  {
+    console.log(this.myForm);
+  }
 }
