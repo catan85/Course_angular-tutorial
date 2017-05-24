8ad3fb360c848a8ffb86ba14ddd3d0bbfb694cc5
ancattaneo <ancattaneo@marzoli.it>
2017-05-24 13:33:43 +0200

    sovrascirve tutti i controlli contenuti in un form tramite setValue e sovrascrive solo acluni di essi tramite patchValue

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') form: NgForm;
+  @ViewChild('f') signupForm: NgForm;
   defaultQuestion='teacher';
 
   genders = [ 'male', 'female']
@@ -14,10 +14,27 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // in questo modo sovrasrivo tutte le proprietá del form
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: 'a@b.it'
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+    // in questo modo posso scrivere solo alcune proprietá
+    this.signupForm.form.patchValue({
+      userData:{
+          username: 'suggestedName'
+      }
+    });
+
   }
 
   onSubmit(){
-      console.log(this.form);
+      console.log(this.signupForm);
   }
 
 
