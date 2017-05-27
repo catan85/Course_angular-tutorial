5225547167baf41db46109dbc6cad89ec67866eb
catan85 <catan85@gmail.com>
2017-05-27 16:55:25 +0200

    iscrizione al cambio di valori e stato del form

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
+import { Observable } from 'rxjs/Observable'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,12 +18,22 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData' : new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email' : new FormControl(null, [Validators.required, Validators.email]),
+        'email' : new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
 
       'gender' : new FormControl('male'),
       'hobbies': new FormArray([])
     });
+
+    // iscrizione al cambio di valori del form
+    //this.signupForm.valueChanges.subscribe(
+    //  (value) => console.log(value)
+    //);
+
+    // iscrizione al cambio di stato del form
+    this.signupForm.statusChanges.subscribe(
+      (status) => (console.log(status))
+    );
   }
 
   onSubmit()
@@ -35,12 +46,27 @@ export class AppComponent implements OnInit {
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
+  // Valitatore customizzato SINCRONO
   forbiddenNames(control: FormControl) : {[s: string]:boolean}
   {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1){
       return {'nameIsForbidden' : true};
     }
     return null;
+  }
+
+  // Validatore custom ASINCRONO
+  forbiddenEmails(control: FormControl) : Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve,reject) => {
+      setTimeout(()=> {
+        if (control.value === 'test@test.com'){
+          resolve({'emailIsForbidden' : true});
+        }else{
+          resolve(null);
+        }
+      }, 2000)
+    });
+    return promise;
   }
 
 }
