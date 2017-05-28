ecb21478260fa2a301124aed319b2aeade3266ff
catan85 <catan85@gmail.com>
2017-05-28 17:34:20 +0200

    editing ingredienti tramite form template driven approach

--- recipes-app/src/app/shopping-list/shopping-list.service.ts
+++ recipes-app/src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -10,6 +11,11 @@ export class ShoppingListService {
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  getIngredient(index:number)
+  {
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {
@@ -23,5 +29,19 @@ export class ShoppingListService {
     // }
     this.ingredients.push(...ingredients);
     this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient){
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number)
+  {
+    if (index > -1)
+    {
+      this.ingredients.splice(index,1)
+      this.ingredientsChanged.next(this.ingredients.slice());
+    }
   }
 }
