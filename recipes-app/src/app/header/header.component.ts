6236c9ee30df733e817f879a4cdfcea181b00276
ancattaneo <ancattaneo@marzoli.it>
2017-06-01 13:37:58 +0200

    implementa salvataggio ricette via http

--- recipes-app/src/app/header/header.component.ts
+++ recipes-app/src/app/header/header.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
-
+import { DataStorageService }from '../shared/data-storage.service';
+import { Response } from '@angular/http';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  constructor (private dataStorageService: DataStorageService){}
+  onSaveData (){
+    console.log('saving');
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response:Response)=>{console.log(response)}
+      );
+  }
 }
