b1993a6299621727adb44fc955ac72f3daf9a2c8
ancattaneo <ancattaneo@marzoli.it>
2017-06-05 13:21:44 +0200

    implementa lazy loading sulla pagina delle ricette

--- recipes-app/src/app/app.module.ts
+++ recipes-app/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
@@ -14,18 +13,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AppRoutingModule,
     
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     SharedModule
