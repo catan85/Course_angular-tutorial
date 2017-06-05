73b05d043447501ed984a4f8e27d1f46c640ad01
catan85 <catan85@gmail.com>
2017-06-05 18:34:42 +0200

    creato core module

--- recipes-app/src/app/app.module.ts
+++ recipes-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoreModule } from './core/core.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
@@ -5,21 +6,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
 import { AppRoutingModule } from './app-routing.module';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -28,9 +21,11 @@ import { HomeComponent } from './home/home.component';
     
     ShoppingListModule,
     AuthModule,
-    SharedModule
+    SharedModule,
+
+    CoreModule
   ],
-  providers: [ShoppingListService,RecipeService, DataStorageService, AuthService, AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
