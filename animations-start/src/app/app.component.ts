5fafb2feb021cda95fa46aadf54f0d534260df77
ancattaneo <ancattaneo@marzoli.it>
2017-06-06 13:36:16 +0200

    inizio progetto animazioni (facile e wild)

---
+++ animations-start/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import 
+{ Component,
+	trigger,
+	state,
+	style,
+	transition,
+	animate
+} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+	animations: [
+
+
+		trigger('divState',[
+			state('normal', style({
+				'background-color': 'red',
+				transform: 'translateX(0)'
+			})),
+			state('highlighted', style({
+				'background-color': 'blue',
+				transform: 'translateX(100px)'
+			})),
+			transition('normal <=> highlighted', animate(300)),
+			//transition('highlighted => normal', animate(800))
+		]),
+
+
+		trigger('wildState',[
+			state('normal', style({
+				'background-color': 'red',
+				transform: 'translateX(0) scale(1)'
+			})),
+			state('highlighted', style({
+				'background-color': 'blue',
+				transform: 'translateX(100px) scale(1)'
+			})),
+			state('shrunken', style({
+				'background-color': 'green',
+				transform: 'translateX(0) scale(0.5)'
+			})),
+			transition('normal => highlighted', animate(300)),
+			transition('highlighted => normal', animate(800)),
+			transition('shrunken <=> *', animate(500))
+		])
+	]
+})
+export class AppComponent {
+
+	list = ['Milk', 'Sugar', 'Bread'];
+	state = 'normal';
+	wildState = 'normal';
+
+	onAdd(item) {
+		this.list.push(item);
+	}
+
+	onDelete(item) {
+		this.list.splice(this.list.indexOf(item), 1);
+	}
+
+	onAnimate(){
+		this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+		this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+	}
+
+	onShrink(){
+		this.wildState = 'shrunken';
+	}
+
+}
